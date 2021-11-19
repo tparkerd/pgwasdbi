4ff4f4960f17df8a50cb65dbe1602049816e3597
Tim Parker <Tim.ParkerD@gmail.com>
2021-11-18 20:45:09 -0600

    Pre-CLI refactor

--- pgwasdbi/cli.py
+++ pgwasdbi/cli.py
@@ -1,23 +1,35 @@
 """Console script for pgwasdbi."""
+
 import argparse
 import logging
 import os
 import sys
-from importlib.metadata import version
-from datetime import datetime as dt
-from pathlib import Path
 from pprint import pformat
 
-from pgwasdbi import pgwasdbi
 from pgwasdbi import log
+from pgwasdbi.core import experiment, pipeline
+from pgwasdbi.util.database import connect
 from pgwasdbi.util.validate import validate
 
-__version__ = version("pgwasdbi")
+from pgwasdbi import __version__
 
 
-def main():
+def parse_options():
     """Function to parse user-provided options from terminal"""
-    description = """Importation script specificaly for Plant GWAS database."""
+    description = """Importation script specifically for Plant GWAS database."""
+    # Superficial pass at runtime arguments
+    ## Database connection test
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-v", "--verbose", action="store_true", help="Increase output verbosity")
+    parser.add_argument("-V", "--version", action="version", version='%(prog)s 1.0.0')
+    parser.add_argument("-n", "--dry-run", dest="dryrun", action="store_true", default=False, help="(Dry Run) Preview output via stdout. Will prevent any files from being written.")
+
+    args = parser.parse_args()
+    print("Parsed args")
+    sys.exit(0)
+
+    
+    
     parser = argparse.ArgumentParser(
         description=description, formatter_class=argparse.RawTextHelpFormatter
     )
@@ -101,6 +113,42 @@ def main():
         args.validate = [v for v in args.validate if v in validation_options]
 
     pgwasdbi.main(args)
+
+
+def run(args):
+    print("==========================\n\nDo the thing, Zhu Li!\n\n==========================")
+    try:
+        experiment.design(args)
+        pipeline.design(args)
+        experiment.collect(args)
+        pipeline.collect(args)
+    except Exception as err:
+        logging.error(f'{type(err).__name__} - {err}'.replace('\n\n', '\t'))
+        raise
+    else:
+        logging.info(f'Importation successful!')
+
+    logging.debug(pformat(args.conf))
+
+def main():
+    try:
+        args = parse_options()
+        args.conn = connect(args)
+    except Exception as err:
+        raise err
+    else:
+        args.fp = os.path.realpath(args.path[0].name)
+        args.cwd = os.path.dirname(os.path.abspath(args.fp))
+        logging.info(f"Processing '{args.fp}'.")
+        if args.validate is not None:
+            try:
+                validate(args)
+            except Exception as err:
+                logging.error(f"ARGS: {pformat(args.conf)}")
+                raise err
+            else:
+                if args.dryrun is not True:
+                    run(args)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
